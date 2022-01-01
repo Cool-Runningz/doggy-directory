f587b15a70ffe4bf978f2f103070bae4ff37b358
Alyssa Holland <alysam0071@gmail.com>
2022-01-01 10:24:49 -0500

    Rename 'default view' to 'landing page'

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders the default view', () => {
+test("renders the landing page", () => {
   render(<App />);
 });
